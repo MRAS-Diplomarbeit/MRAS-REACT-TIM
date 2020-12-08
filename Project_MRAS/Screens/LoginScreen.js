c51df1f5adffcf9287f3ed9257c12eef38f9443a
Kondert Tim <43296753+KondertTim@users.noreply.github.com>
2020-12-08 17:03:16 +0100

    Login Nav

--- Project_MRAS/Screens/LoginScreen.js
+++ Project_MRAS/Screens/LoginScreen.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Component } from 'react';
+import { useState } from 'react';
 import { StyleSheet,Button, View, Text,TextInput,ImageBackground,Image,SafeAreaView } from 'react-native';
 import { TouchableOpacity } from 'react-native-gesture-handler';
 import AppButton from '../Components/Buttons/AppButton';
@@ -10,9 +10,12 @@ import styles from "../Components/styles";
 
 
 
-   const Login = ({navigation}) => (
+   const Login = ({navigation}) => {
 
-    
+    const [userName,setUsername] = useState("userName");
+    const [userPassword,setuserPassword] = useState("userPassword");
+
+    return(
           <ImageBackground blurRadius={5} style={styles.background} source={require("../assets/hintergrund.jpg")}>
             
 
@@ -21,12 +24,12 @@ import styles from "../Components/styles";
               <Text style={styles.frontpageheading}>Multiroom Audio Setup</Text>
               <Image fadeDuration={2500} source={require('../assets/logo2.png')}/>
               <Text style={styles.frontpageheading}>MRAS</Text>
-              <TextInput style={styles.textInput} placeholder="Username"/>
-              <TextInput style={styles.textInput} placeholder="Password"/>  
+              <TextInput style={styles.textInput} placeholder="Username" onChangeText={text=>setUsername(text)}/>
+              <TextInput style={styles.textInput} secureTextEntry={true} placeholder="Password" onChangeText={text=>setuserPassword(text)}/>  
 
              
 
-              <AppButton color = "secondary" title="Login" onPress={() => navigation.navigate("Home")}/>
+              <AppButton color = "secondary" title="Login" onPress={() => tryToLogin(navigation,userName,userPassword)}/>
               
               
               <AppButton title="Register" onPress={() => navigation.navigate("Register")}/>
@@ -39,10 +42,26 @@ import styles from "../Components/styles";
           
           </ImageBackground>
    );
+  }
 
 
 
 export default Login;
+
+const tryToLogin = (navigation,userName,userPassword) =>{
+  if((userName=="Test")&&(userPassword=="Cisco"))
+  {
+    navigation.navigate("Home");
+  
+  }
+  else
+  {
+    alert("Wrong Username or Password");
+  }
+}
+
+
+
 
 
 
--- Project_MRAS/Screens/RegisterScreen.js
+++ Project_MRAS/Screens/RegisterScreen.js
@@ -1,34 +1,60 @@
 import * as React from 'react';
-import { StyleSheet,Button, View, Text } from 'react-native';
+import { StyleSheet,Image, SafeAreaView, Text, ImageBackground } from 'react-native';
 import { TextInput } from 'react-native-gesture-handler';
+import styles from "../Components/styles";
+import AppButton from '../Components/Buttons/AppButton';
+import { useState } from 'react';
+import DeviceInfo from 'react-native-device-info';
 
 
-const Register = ({navigation}) => (
 
+
+
+
+const Register = ({navigation}) => {
+
+    const [userName,setUsername] = useState("userName");
+    const [userPassword,setuserPassword] = useState("userPassword");
+    const [userPasswordConf,setuserPasswordConf] = useState("userPasswordConf");
+
+
+    return(
+    <ImageBackground blurRadius={5} style={styles.background} source={require("../assets/hintergrund2.jpg")}>
     
-  <ImageBackground blurRadius={5} style={styles.background} source={require("../assets/hintergrund2.jpg")}>
-    
 
-    <SafeAreaView style={styles.container}>
+      <SafeAreaView style={styles.container}>
 
-      <Text style={styles.frontpageheading}>Multiroom Audio Setup</Text>
-      <Image fadeDuration={2500} source={require('../assets/logo2.png')}/>
-      <Text style={styles.frontpageheading}>MRAS</Text>
-      <TextInput style={styles.textInput} placeholder="Username"/>
-      <TextInput style={styles.textInput} placeholder="Password"/>  
-      <TextInput style={styles.textInput} placeholder="Confirm Password" />
+        <Text style={styles.frontpageheading}>Multiroom Audio Setup</Text>
+      
+      
+        <TextInput style={styles.textInput} placeholder="Username" onChangeText={text=>setUsername(text)}/>
+        <TextInput style={styles.textInput} placeholder="Password" onChangeText={text=>setuserPassword(text)}/>  
+        <TextInput style={styles.textInput} placeholder="Confirm Password" onChangeText={text=>setuserPasswordConf(text)}/>
      
       
-      <AppButton title="Register" onPress={() => navigation.navigate("Home")}/>
+        <AppButton title="Register" onPress={() => tryToRegister(navigation,)}/>
 
 
-    </SafeAreaView>
-
+      </SafeAreaView>
+    </ImageBackground>
+    );
     
   
-  
-  </ImageBackground>
-);
+}
 
 export default Register;
 
+const tryToRegister = (navigation,userName,userPassword,userPasswordConf) =>{
+  if(userName!="Test")
+  {
+    if(userPassword==userPasswordConf)
+    {
+      navigation.navigate("Home");
+    }
+  
+  }
+  else
+  {
+    alert(DeviceInfo.getMacAddress());
+  }
+}
